0cb538373195d392785f19de2f5866abde576234
Ariel Adams <ariel.talbot@iconfitness.com>
2021-12-21 14:32:02 -0700

    shuffle and restart features, fixed bug with card being able to match with itself

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,17 @@
       />
     </section>
     <h2>{{ status }}</h2>
+    
+    <div class="grid-container">
+      <button class="shuffle" @click="shuffleCards" role="button">Shuffle Cards</button>
+      <button class="restart" @click="restartGame" role="button">Restart Game</button>
+    </div>
+
   </div>
 </template>
 
 <script>
+import _ from 'lodash' 
 import { computed, ref, watch } from 'vue'
 import Card from './components/Card'
 export default {
@@ -44,19 +51,57 @@ export default {
       return remainingCards / 2
     })
 
-    for (let i = 0; i < 16; i++) {
-      cardList.value.push({
-        value: i,
-        visible: false,
-        position: i,
-        matched: false
+    const shuffleCards = () => {
+      cardList.value = _.shuffle(cardList.value);
+    }
+
+    const restartGame = () => {
+      shuffleCards()
+
+      cardList.value = cardList.value.map((card, index) => {
+        return {
+          ...card,
+          matched: false,
+          position: index,
+          visible: false
+        }
       })
     }
+
+    // create matching card pair
+    const cardItems = [1, 2, 3, 4, 5, 6, 7, 8]
+    
+    cardItems.forEach(item => {
+      cardList.value.push({
+        value: item,
+        visible: false,
+        position: null,
+        matched: false
+      })
+      cardList.value.push({
+        value: item,
+        visible: false,
+        position: null,
+        matched: false
+      })
+    })
+
+    cardList.value = cardList.value.map((card, index) => {
+      return {
+        ...card,
+        position: index
+      }
+    })
+    
     const flipCard = payload => {
       cardList.value[payload.position].visible = true
 
       if(userSelection.value[0]){
-        userSelection.value[1] = payload
+        if((userSelection.value[0].position === payload.position) && (userSelection.value[0].faceValue === payload.faceValue)){
+          return
+        } else {
+          userSelection.value[1] = payload
+        }
       } else {
         userSelection.value[0] = payload
       }
@@ -69,15 +114,17 @@ export default {
 
         // check to see if cards match
         if(cardFirst.faceValue === cardSecond.faceValue){
-          status.value = "It's a match!"
+          
           cardList.value[cardFirst.position].matched = true
           cardList.value[cardSecond.position].matched = true
+
         } else {
-          status.value = "Sorry, try again"
           
           // flip cards back to initial position
-          cardList.value[cardFirst.position].visible = false
-          cardList.value[cardSecond.position].visible = false
+          setTimeout(() => {
+            cardList.value[cardFirst.position].visible = false
+            cardList.value[cardSecond.position].visible = false
+          }, 2000)
         }
 
         userSelection.value.length = 0
@@ -88,7 +135,9 @@ export default {
       cardList,
       flipCard,
       userSelection,
-      status
+      status,
+      shuffleCards,
+      restartGame
     }
   }
 }
@@ -103,6 +152,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 .board {
   display: grid;
   grid-template-columns: 100px 100px 100px 100px;
@@ -110,5 +160,80 @@ export default {
   grid-column-gap: 30px;
   grid-row-gap: 30px;
   justify-content: center;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto auto;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+
+button {
+  align-items: center;
+  border: 2px solid #111;
+  border-radius: 8px;
+  box-sizing: border-box;
+  color: #111;
+  cursor: pointer;
+  display: flex;
+  font-family: Inter,sans-serif;
+  font-size: 16px;
+  height: 48px;
+  justify-content: center;
+  line-height: 24px;
+  max-width: 100%;
+  padding: 0 25px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  margin: 0 auto;
+}
+
+button.shuffle {
+  background-color: #BBD4ED;
+}
+
+button.restart {
+  background-color: #dde6dd;
+}
+
+button:after {
+  background-color: #111;
+  border-radius: 8px;
+  content: "";
+  display: block;
+  height: 48px;
+  left: 0;
+  width: 100%;
+  position: absolute;
+  top: -2px;
+  transform: translate(8px, 8px);
+  transition: transform .2s ease-out;
+  z-index: -1;
+}
+
+button:hover:after {
+  transform: translate(0, 0);
+}
+
+button:active {
+  background-color: #ffdeda;
+  outline: 0;
+}
+
+button:hover {
+  outline: 0;
+}
+
+@media (min-width: 768px) {
+  button {
+    padding: 0 40px;
+  }
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -35,7 +35,7 @@ export default {
 <template>
   <div class="card" @click="selectCard">
     <div v-if="visible" class="card-face front">
-      {{ value }} - {{ matched }}
+      {{ value }} - {{ position }} - {{ matched }}
     </div>
     <div v-else class="card-face back">
       Back
